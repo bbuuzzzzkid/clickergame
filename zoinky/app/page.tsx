753a160922d82364b59563020be378d86a13665d
bbuuuzzzzkid <rohanslamba@gmail.com>
2026-07-02 15:52:48 -0700

    Dog park glitchfix WEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- zoinky/app/page.tsx
+++ zoinky/app/page.tsx
@@ -45,21 +45,14 @@ const operationGetTheBackground = (currentArea:Area)=>{
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div>
-           <h1>WEEEEE SAFARI THAT HAS WEIRD STUFF IN IT ANYWAY WHy ARE YOU HERE'</h1>
-
-          {/* <SchoolIsBad message = {
-  "SCHOOL NEEDS AN UPDATE AND LESS HOMEWORK BECAUSE HOMEWORK SUCKS CLICK THIS BUTTON IF YOU AGREE!!!!!!!!!!!!!"
-          }
-          onClickHandler={() => alert("YAY! YOU THINK HOMEWORK IS BAD TOO! BECAUSE IS IT SUCKS!!! SPREAD THE WORD!!!")}
-          ></SchoolIsBad> */}
-          <WeirdButton destination = "/clicker" clickNumber={100} label = "OOHHH BUTTON :)" label2 = "YOU HAVE CLICKED THE BUTTON THIS MANY TIMES, WHILE WASTING YOUR SHORT LIFESPAN IN THE PROCESS:" startmessage = "OOHHH BUTTON! :)" endmessage = "OOHHHH FANCY HYPERLINK WEIRD PAGE CHANGE THINGY :)"></WeirdButton>
-        </div>
+           <h1>WEEEEE SAFARI THAT HAS WEIRD STUFF IN IT ANYWAY WHy ARE YOU HERE do you have a LIFE if you dont I am very sorry for your loss, unless you are a robot in which case whoever programmed you is high.</h1>
+          </div>
          <Image src = {GETALIFE}
          width = {1000} height = {1000} alt = "GET A LIFE"
          />
 
         <button onClick = {()=> setStartgame(true)}>START GAME</button>
-
+{operationGetTheBackground(ASafari.current.thisArea)}
       </main>
     </div>
   );
