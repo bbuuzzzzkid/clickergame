32e1a575fb840a7e74af1a7a97bdec62f9eb40bd
bbuuuzzzzkid <rohanslamba@gmail.com>
2026-07-09 18:30:55 -0700

    PGHiprewgopisgeOIwegspowg (NO TIME FOR THIS COMMENT)

--- zoinky/app/page.tsx
+++ zoinky/app/page.tsx
@@ -26,7 +26,7 @@ return <div>
   <h1>YOU HAVE STARTED THE GAME. Bro, you are just wasting your time like what the actual hell man</h1> 
   <button onClick = {()=>{
   ASafari.current.handleTransition()
-  
+  ASafari.current.timerLoopThatRunsEveryFrame()
   console.log(ASafari.current.thisArea)
   setThisWasMadeToFixABug(ASafari.current.thisArea)   
   }} className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5 w-128" >BUTTON 1 YIPEE</button> 
