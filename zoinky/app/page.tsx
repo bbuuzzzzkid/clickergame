893bcd81a4baa8305aa6a1c31446bb4ca9942c0f
bbuuuzzzzkid <rohanslamba@gmail.com>
2026-07-16 17:44:01 -0700

    as;gharwil;

--- zoinky/app/page.tsx
+++ zoinky/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
 const canvasRef = useRef(null)
 
   return (
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <canvas id="c" ref = {canvasRef} className="w-full h-screen bg-red"></canvas>
+      <main>
+        <canvas id="c" ref = {canvasRef} className="w-full h-screen bg-blue"></canvas>
       </main>
   );
 }
